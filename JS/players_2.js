6f236f63542bc34f5c9340e1d8cc3874be22b359
Adel Kalu <37424831+Adel-Kalu@users.noreply.github.com>
2021-03-08 19:10:21 +0300

    Add files via upload

---
+++ JS/players_2.js
@@ -0,0 +1,283 @@
+var clicked = false;
+var num = 0;
+//randomNumber on click dice 
+const randomNumber = function () {
+    if (!clicked) {
+
+        var audio = document.getElementById('audio');
+        audio.play();
+
+        num = Math.floor((Math.random() * 6) + 1);
+        $('#dice').css('background-image', 'url(Images/' + num + '.png)');
+        clicked = true;
+    }
+    if (num != 6 && notFree()) {
+        var bad = document.getElementById('bad');
+        bad.innerText = "Unfortunatlly you stuck";
+        setTimeout(changePlayer, 1000);
+        clicked = false;
+    }
+}
+$('#dice').on('click', randomNumber);
+
+// Object for check do not have tablet in board 
+var positions = {
+    redTablet1: 0, redTablet2: 0, redTablet3: 0, redTablet4: 0,
+    yellowTablet1: 0, yellowTablet2: 0, yellowTablet3: 0, yellowTablet4: 0
+};
+var onboard = {
+    redTablet1: 0, redTablet2: 0, redTablet3: 0, redTablet4: 0,
+    yellowTablet1: 0, yellowTablet2: 0, yellowTablet3: 0, yellowTablet4: 0
+};
+function notFree() {
+    var text = document.getElementById('player');
+    for (var i = 1; i <=4; i++) {
+        if (onboard[text.style.color + "Tablet" + i] == 1 || positions[text.style.color + "Tablet" + i]+num>=60) return false;
+    }
+    return true;
+}
+
+var player1 = prompt("Please enter player1 name (red) : ");
+var player2 = prompt("Please enter player2 name (yellow) : ");
+
+
+$('#player').text(player1);
+
+// change player turns
+function changePlayer() {
+    if (num != 6){
+    var text = document.getElementById('player');
+    switch (text.innerText) {
+        case player1: text.innerText = player2; text.style.color = "yellow"; break;
+        case player2: text.innerText = player1; text.style.color = "red"; break;
+       
+    }
+    }
+    var bad = document.getElementById('bad');
+    bad.innerText = "";
+    $('#dice').css('background-image', 'url(Images/dice1.gif)');
+}
+
+
+//Tablet move
+
+var currPos = 0;
+var currcolor = "";
+var NumOfPaw = "";
+var currpawn = "";
+
+function randomMove(Color, paw) {
+    var text = document.getElementById('player');
+    NumOfPaw = paw;
+    currcolor = Color;
+    currpawn = currcolor + "Tablet" + NumOfPaw;
+    currPos = positions[currpawn];
+    if (num + currPos > 60) {
+        Stuck();
+    }
+    else {
+        if (clicked) {
+
+            var audioRoll = document.getElementById('audio-roll');
+            audioRoll.play();
+
+            var position = currPos;
+            if (text.style.color == currcolor) {
+                if (onboard[currpawn] === 1 || num === 6) {
+                    if (onboard[currpawn] === 0) {
+                        var doc = document.getElementById(currpawn);
+                        switch (Color) {
+                            case "red":
+                                doc.style.left = 206 + 'px';
+                                doc.style.top = 440 + "px";
+                                break;
+
+                            case "yellow":
+                                doc.style.left = 272 + 'px';
+                                doc.style.top = 44 + "px";
+                                break;
+                        }
+                        onboard[currpawn] = 1;
+                    }
+                    else {
+                        switch (Color) {
+                            case "red":
+                                for (i = currPos; i < position + num; i++) {
+                                    stepsRed[i]();
+                                }
+                                break;
+
+                            case "yellow":
+                                for (i = currPos; i < position + num; i++) {
+                                    stepsYellow[i]();
+                                }
+                                break;
+
+                        }
+
+                        positions[currpawn] = currPos;
+                        var victim = HaveHover();
+                        if (victim != false) {
+                            ResetPawn(victim);
+                        }
+                        // check for winner
+                        if (currPos == 60) { pawnOut[currcolor]++; onboard[currpawn] = 0; positions[currpawn] = 0; document.getElementById(currpawn).style.visibility = "hidden"; };
+                        CheckForWinner();
+                        changePlayer();
+                    }
+                    num = 0;
+                    clicked = false;
+                    $('#dice').css('background-image', 'url(Images/dice1.gif)');
+
+                }
+                else Stuck();
+            }
+        }
+    }
+}
+
+
+// function for stuck
+
+function Stuck() {
+    if (onboard[currpawn] == 0||currPos+num>60) {
+        if (notFree()||currPos+num>60) {
+            var bad = document.getElementById('bad');
+            bad.innerText = "Unfortunatlly you stuck";
+            clicked = false;
+            $('#dice').css('background-image', 'url(Images/dice1.gif)');
+            setTimeout(changePlayer, 1000);
+        }
+    }
+}
+
+
+// check if come to in one plase
+
+var allcolor = ["red", "yellow"];
+
+function HaveHover() {
+    var count = 0;
+    var toKill = "";
+    for (var i = 0; i < allcolor.length; i++) {
+        for (var n = 1; n <= 4; n++) {
+            var first = document.getElementById(allcolor[i] + "Tablet" + n);
+            var second=document.getElementById(currpawn);
+            if (first.style.top==second.style.top&&first.style.left==second.style.left&&currcolor!=allcolor[i]&&currPos+num<60) {
+                count++;
+                toKill = allcolor[i] + "Tablet" + n;
+                return toKill;
+            }
+        }
+    }
+    return false;
+}
+
+// check for winner and display the winner
+
+var pawnOut = {red:0,yellow:0}
+
+function CheckForWinner() {
+    if (pawnOut[currcolor] == 4) {
+        var dice = document.getElementById("dice");
+        var player = document.getElementById("player");
+        dice.innerText = "";
+        dice.style.visibility = "hidden";
+        player.innerText = "The Winner is the "+ currcolor + " player";
+        
+        var audio = document.getElementById('audio-winner');
+        audio.play();
+        
+    }
+}
+
+
+
+//reset tablet if come to in one plase kill the first
+function ResetPawn(victim) {
+    onboard[victim] = 0;
+    positions[victim] = 0;
+    var pawnToMove = document.getElementById(victim);
+
+    var audio = document.getElementById('audio-kill');
+    audio.play();
+
+    switch (victim) {
+        case "redTablet1": pawnToMove.style.top = 423 + "px"; pawnToMove.style.left = 58 + "px"; break;
+        case "redTablet2": pawnToMove.style.top = 423 + "px"; pawnToMove.style.left = 124 + "px"; break;
+        case "redTablet3": pawnToMove.style.top = 358 + "px"; pawnToMove.style.left = 58 + "px"; break;
+        case "redTablet4": pawnToMove.style.top = 358 + "px"; pawnToMove.style.left = 124 + "px"; break;
+
+        case "yellowTablet1": pawnToMove.style.top = 127 + "px"; pawnToMove.style.left = 355 + "px"; break;
+        case "yellowTablet2": pawnToMove.style.top = 127 + "px"; pawnToMove.style.left = 421 + "px"; break;
+        case "yellowTablet3": pawnToMove.style.top = 60 + "px"; pawnToMove.style.left = 355 + "px"; break;
+        case "yellowTablet4": pawnToMove.style.top = 60 + "px"; pawnToMove.style.left = 421 + "px"; break;
+
+    }
+}
+
+// move tablets
+
+var step = 33;
+var stepsRed = [];
+var stepsYellow = [];
+var stepsBlue =[];
+var stepsGreen =[];
+function pushSteps(value, steps, count) {
+    for (i = 0; i < count; i++) steps.push(value);
+}
+
+//red tablet path
+pushSteps(stepUp, stepsRed,5);
+pushSteps(stepLeft, stepsRed,6);
+pushSteps(stepUp, stepsRed,2);
+pushSteps(stepRight, stepsRed,6);
+pushSteps(stepUp, stepsRed,6);
+pushSteps(stepRight, stepsRed,2);
+pushSteps(stepDown, stepsRed,6);
+pushSteps(stepRight, stepsRed,6);
+pushSteps(stepDown, stepsRed,2);
+pushSteps(stepLeft, stepsRed,6);
+pushSteps(stepDown, stepsRed,6);
+pushSteps(stepLeft, stepsRed,1);
+pushSteps(stepUp, stepsRed,6);
+
+//Yellow tablet path
+pushSteps(stepDown,stepsYellow,5);
+pushSteps(stepRight, stepsYellow,6);
+pushSteps(stepDown, stepsYellow,2);
+pushSteps(stepLeft, stepsYellow,6);
+pushSteps(stepDown, stepsYellow,6);
+pushSteps(stepLeft, stepsYellow,2);
+pushSteps(stepUp, stepsYellow,6);
+pushSteps(stepLeft, stepsYellow,6);
+pushSteps(stepUp, stepsYellow,2);
+pushSteps(stepRight, stepsYellow,6);
+pushSteps(stepUp, stepsYellow,6);
+pushSteps(stepRight, stepsYellow,1);
+pushSteps(stepDown, stepsYellow,6);
+
+function stepDown() {
+    var doc = document.getElementById(currcolor + "Tablet" + NumOfPaw);
+    var curr = Number(doc.style.top.replace(/[a-z]/g, ''));
+    doc.style.top = (curr+step)+'px';
+    currPos++;
+}
+function stepUp() {
+    var doc = document.getElementById(currpawn);
+    var curr = Number(doc.style.top.replace(/[a-z]/g, ''));
+    doc.style.top = (curr - step) + 'px';
+    currPos++;
+}
+function stepLeft() {
+    var doc = document.getElementById(currpawn);
+    var curr = Number(doc.style.left.replace(/[a-z]/g, ''));
+    doc.style.left = (curr - step) + 'px';
+    currPos++;
+}
+function stepRight() {
+    var doc = document.getElementById(currpawn);
+    var curr = Number(doc.style.left.replace(/[a-z]/g, ''));
+    doc.style.left = (curr + step) + 'px';
+    currPos++;
+}
--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# Ludo-King
+# Ludo-King Game
+
+## Instructions
+Two or four can play, without partnerships. At the beginning of the game, each player's four tokens are out of play and staged in the player's yard (one of the large corner areas of the board in the player's colour). When able to, the players will enter their tokens one per turn on their respective starting squares, and proceed to race them clockwise around the board along the game track . When reaching the square below his home column, a player continues by moving tokens up the column to the finishing square. The first to bring all their tokens to the finish wins the game. The others often continue to play to determine second, third, and fourth-place finishers.
+
+### technologies used :
+* html & css.
+* JavaScript .
+* jQuery and vanilla javascript for DOM manipulation.
+### Wireframe
+![Wireframe](Images/Wireframe.png)
+
+### Planning
+First create html, css and javaScript files. Then design the html ludo-king pages. After that I work on background posts. Finally, get the user experience.
+
+### User Stories
+* As a player I want to add my name so that I can Know it is my turn. 
+* As a player I want to able continue  play so that we can determine second and third winner.
+* As a player I want to add 2 and 4 players so that I can play with my friends.
+* As a player I want to able kill pawn players so that it makes playing more exciting.
+* As a player I want to add 3 players so that we can play if we are 3 only.
+
+### Problem-solving strategy
+1. Google it.
+2. Console.log or alert.
+
+### solved for the winner
+The player who delivers all the pawns to the endpoint wins.
+
+```javascript
+// check for winner and display the winner 
+var pawnOut = {red:0,blue:0,green:0,yellow:0}
+function CheckForWinner() {
+    if (pawnOut[currcolor] == 4) {
+        var dice = document.getElementById("dice");
+        var player = document.getElementById("player");
+        dice.innerText = "";
+        dice.style.visibility = "hidden";
+        player.innerText = "The Winner is the "+ currcolor + " player";
+    }
+}
+```
+
+### favorite functions work
+My favorite job is the random number that describes that it must change from one player to another every time a player does not get a dice equal to six and change the number of dice.
+
+```javascript
+//randomNumber on click dice 
+const randomNumber = function () {
+    if (!clicked) {
+
+        var audio = document.getElementById('audio');
+        audio.play();
+
+        num = Math.floor((Math.random() * 6) + 1);
+        $('#dice').css('background-image', 'url(Images/' + num + '.png)');
+        clicked = true;
+    }
+    if (num != 6 && notFree()) {
+        var bad = document.getElementById('bad');
+        bad.innerText = "Unfortunatlly you stuck";
+        setTimeout(changePlayer, 1000);
+        clicked = false;
+    }
+}
+$('#dice').on('click', randomNumber);
+```
+
+### Resource
+[SIMPLE LUDO GAME IN JAVASCRIPT WITH SOURCE CODE](https://code-projects.org/simple-ludo-game-in-javascript-with-source-code/)
